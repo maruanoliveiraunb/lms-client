a0279c3d34105b1e5b976b8a05f6259573e78b9b
C1316834 Maruan Cena Oliveira <c1316834@interno.bb.com.br>
2021-05-26 15:06:38 -0300

    teste para uri da api

--- src/configs.js
+++ src/configs.js
@@ -2,6 +2,6 @@ const API_PATH = process.env.API_PATH || 'http://localhost:3002';
 
 export default {
     api: {
-        server: API_PATH
+        server: 'https://lms-server-ia.herokuapp.com'
     }
 }
