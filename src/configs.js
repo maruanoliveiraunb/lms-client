f25409d2ee14ec3237b135f24cb74b827bad8f7d
C1316834 Maruan Cena Oliveira <c1316834@interno.bb.com.br>
2021-05-26 15:01:27 -0300

    Change api path

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,7 @@
+const API_PATH = process.env.API_PATH || 'http://localhost:3002';
+
 export default {
     api: {
-        server: process.env.API_PATH || 'http://localhost:3002'
+        server: API_PATH
     }
 }
