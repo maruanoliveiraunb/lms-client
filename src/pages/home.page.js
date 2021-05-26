b05dcbd4f470b34a95c981b0d9cb20e40dfb1696
C1316834 Maruan Cena Oliveira <c1316834@interno.bb.com.br>
2021-05-26 15:40:03 -0300

    Teste para capturar env

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -4,10 +4,13 @@ import {Container, Typography} from "@material-ui/core";
 
 class HomePage extends React.Component {
 
+
     render() {
+    const envTeste = process.env;
         return (
             <Container>
                 <Typography variant="h2">LMS CLIENT</Typography>
+                { JSON.stringify(envTeste) }
             </Container>
         );
     }
