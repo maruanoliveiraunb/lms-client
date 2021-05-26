d159b6fee38ab8d29f07f55b1e06db59f45351d8
C1316834 Maruan Cena Oliveira <c1316834@interno.bb.com.br>
2021-05-26 16:53:03 -0300

    env

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -7,10 +7,12 @@ class HomePage extends React.Component {
 
     render() {
     const envTeste = process.env;
+    const teste = process.env.API_PATH;
         return (
             <Container>
                 <Typography variant="h2">LMS CLIENT</Typography>
                 { JSON.stringify(envTeste) }
+                teste: { teste }
             </Container>
         );
     }
